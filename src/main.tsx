09c054010584860b4fd6393e67550964d790dfde
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-12-28 23:09:18 +0200

    Create import root

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
-import "./styles/globals.css";
 import "modern-normalize/modern-normalize.css";
-import "react-hot-toast";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from "react-dom/client";
