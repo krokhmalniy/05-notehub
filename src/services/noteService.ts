5d3e32a8b975ae8383ee891c57616ae4be0602fa
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-12-30 18:28:25 +0200

    Done noteService.ts

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,4 +1,17 @@
 import axios from "axios";
+import type { Note } from "../types/note";
+
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+interface FetchNotesParams {
+  page: number;
+  perPage: number;
+  search?: string;
+  sortBy?: string;
+}
 
 const AUTH_TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
 
@@ -11,3 +24,8 @@ const instance = axios.create({
 });
 
 instance.defaults.headers.common["Authorization"] = `Bearer ${AUTH_TOKEN}`;
+
+async function fetchNotes(params: FetchNotesParams): Promise<FetchNotesResponse> {
+  const response = await instance.get<FetchNotesResponse>("/notes", { params });
+  return response.data;
+}
