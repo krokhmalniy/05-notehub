73477d5a86bb4f0252b866344cc59b27c5950695
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2026-01-09 18:51:42 +0200

    Create pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-// Бібліотеки
+
 import "modern-normalize/modern-normalize.css";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,2 +1,31 @@
+// Бібліотеки
+import ReactPaginate from "react-paginate";
+
 // Стилі
 import css from "./Pagination.module.css";
+
+interface PaginationProps {
+  pageCount: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+function Pagination({pageCount, currentPage, onPageChange}: PaginationProps) {
+
+return (
+  <ReactPaginate
+    pageCount={pageCount}
+    forcePage={currentPage - 1}
+    onPageChange={({ selected }) => onPageChange(selected + 1)}
+    previousLabel="←"
+    nextLabel="→"
+    breakLabel="..."
+    pageRangeDisplayed={3}
+    marginPagesDisplayed={1}
+    containerClassName={css.pagination}
+    activeClassName={css.active}
+  />
+);
+}
+
+export default Pagination;
