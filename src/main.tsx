4b3677882397449c3abfbe2912b90b818ed601e6
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2026-01-09 10:21:12 +0200

    Up to date gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // Бібліотеки
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider,  } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider  } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 
